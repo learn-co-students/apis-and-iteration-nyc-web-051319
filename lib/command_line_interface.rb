2520b9fad876a5919f009ab89b95313911e6f889
chrisyuaners <chrisyuan7@gmail.com>
2019-05-26 01:27:26 -0400

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
   # puts out a welcome message here!
+  puts "Hi. Welcome to the Star Wars Search Engine."
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Please enter a character name:"
   # use gets to capture the user's input. This method should return that input, downcased.
+  character = gets.chomp
+  character
 end
